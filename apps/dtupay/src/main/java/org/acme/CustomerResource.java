ed69afbb03f1fabbb85fdedbb453a2c917b2d382
Ibrahim Bacha <Mik_f@outlook.dk>
2023-01-17 13:18:07 +0100

    clean up

--- apps/dtupay/src/main/java/org/acme/CustomerResource.java
+++ apps/dtupay/src/main/java/org/acme/CustomerResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/customer")
 public class CustomerResource {
